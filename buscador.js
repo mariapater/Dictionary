241ce2f568538cf4b0c9479695fcc1d55dc32023
mariapater <88839510+mariapater@users.noreply.github.com>
2021-08-12 08:47:40 -0500

    Add files via upload

---
+++ buscador.js
@@ -0,0 +1,137 @@
+//HTML
+function abrir_a() {
+    document.getElementById("a").style.display="block";
+}
+function abrir_adress(){
+    document.getElementById("adress").style.display="block";
+}
+function abrir_body(){
+    document.getElementById("body").style.display="block";
+}
+function abrir_button(){
+    document.getElementById("button").style.display="block";
+}
+function abrir_caption(){
+    document.getElementById("caption").style.display="block";
+}
+function abrir_details(){
+    document.getElementById("details").style.display="block";
+}
+function abrir_div(){
+    document.getElementById("div").style.display="block";
+}
+function abrir_em(){
+    document.getElementById("em").style.display="block";
+}
+function abrir_form(){
+    document.getElementById("form").style.display="block";
+}
+function abrir_header(){
+    document.getElementById("header").style.display="block";
+}
+function abrir_html(){
+    document.getElementById("html").style.display="block";
+}
+function abrir_input(){
+    document.getElementById("input").style.display="block";
+}
+function abrir_meta(){
+    document.getElementById("meta").style.display="block";
+}
+function abrir_nav(){
+    document.getElementById("nav").style.display="block";
+}
+//CSS
+function abrir_css1(){
+    document.getElementById("css1").style.display="block";
+}
+function abrir_etiqueta(){
+    document.getElementById("etiqueta").style.display="block";
+}
+function abrir_class(){
+    document.getElementById("class").style.display="block";
+}
+function abrir_id(){
+    document.getElementById("id").style.display="block";
+}
+function abrir_sel1_sel2(){
+    document.getElementById("sel1_sel2").style.display="block";
+}
+function abrir_class1(){
+    document.getElementById("class1").style.display="block";
+}
+function abrir_sell1_class1(){
+    document.getElementById("sel1.class1").style.display="block";
+}
+function abrir_sell1_class1(){
+    document.getElementById("sel1.class1").style.display="block";
+}
+function abrir_px(){
+    document.getElementById("px").style.display="block";
+}
+function abrir_text(){
+    document.getElementById("text").style.display="block";
+}
+function abrir_porcentaje(){
+    document.getElementById("porcentaje").style.display="block";
+}
+function abrir_color(){
+    document.getElementById("color").style.display="block";
+}
+function abrir_image(){
+    document.getElementById("image").style.display="block";
+}
+function abrir_repeat(){
+    document.getElementById("repeat").style.display="block";
+}
+function abrir_position(){
+    document.getElementById("position").style.display="block";
+}
+
+//JavaScript
+
+function abrir_alert(){
+    document.getElementById("alert").style.display="block";
+}
+function abrir_algol(){
+    document.getElementById("algol").style.display="block";
+}
+function abrir_algorithm(){
+    document.getElementById("algorithm").style.display="block";
+}
+function abrir_allocate(){
+    document.getElementById("allocate").style.display="block";
+}
+function abrir_add(){
+    document.getElementById("add").style.display="block";
+}
+function abrir_AOP(){
+    document.getElementById("AOP").style.display="block";
+}
+function abrir_API(){
+    document.getElementById("API").style.display="block";
+}
+function abrir_APK(){
+    document.getElementById("APK").style.display="block";
+}
+function abrir_px(){
+    document.getElementById("px").style.display="block";
+}
+function abrir_applet(){
+    document.getElementById("applet").style.display="block";
+}
+function abrir_argument(){
+    document.getElementById("argument").style.display="block";
+}
+function abrir_array(){
+    document.getElementById("array").style.display="block";
+}
+function abrir_ASCII(){
+    document.getElementById("ASCII").style.display="block";
+}
+function abrir_ASP(){
+    document.getElementById("ASP").style.display="block";
+}
+function abrir_ASPI(){
+    document.getElementById("ASPI").style.display="block";
+}
